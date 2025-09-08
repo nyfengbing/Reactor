b2716f249dc4c1dbbb3bfae7697f31ccb7a29c42
nyfengbing <143155537+nyfengbing@users.noreply.github.com>
2025-09-08 11:21:34 +0800

    Add files via upload

---
+++ 基础epoll服务器/net.hpp
@@ -0,0 +1,18 @@
+#ifndef  NET_H
+#define NET_H
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <netinet/tcp.h>  // 包含 TCP 相关的选项，如 TCP_NODELAY
+#include <sys/epoll.h>
+
+
+#endif  
